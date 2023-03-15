fa4f02d318bc5c428397b285ff66572b4099e261
Rawxia <127301185+Rawxia@users.noreply.github.com>
2023-03-15 12:00:44 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,22 @@
-function closeModal() {
-  document.getElementById("popup-modal").hidden = true;
+const closeModal = () => {
+const popupModal = document.getElementById("popup-modal");
+popupModal.hidden = true;
+};
+
+const denial = () => {
+sendChannel.send(JSON.stringify({ confirmed: false }));
+downloadInProgress = false;
+closeModal();
+};
+
+const accepting = () => {
+sendChannel.send(JSON.stringify({ confirmed: true }));
+closeModal();
+};
+
+window.onclick = event => {
+const modal = document.getElementById("popup-modal");
+if (event.target === modal) {
+closeModal();
 }
-function denial() {
-  sendChannel.send(JSON.stringify({ confirmed: false })); downloadInProgress = false; closeModal();
-}
-function accepting() {
-  sendChannel.send(JSON.stringify({ confirmed: true })); closeModal();
-}
-window.onclick = function (event) {
-  let modal = document.getElementById("popup-modal");
-  if (event.target == modal) { closeModal(); }
 };
